cbc467075c4a9e5d7a64b1e25870a443a90baf55
valemorenodev <valentinamoreno2002@gmail.com>
2025-12-26 10:29:08 -0500

    ejemplo env y adicion de variable de entorno en git ignore

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import 'dotenv/config';
 import app from './app.js';
+import { connectDB } from './config/db.js';
 
 const PORT = process.env.PORT || 3000;
-
+await connectDB();
 app.listen(PORT, () => {
   console.log(`servidor corriendo en: http://localhost:${PORT}`);
 });
